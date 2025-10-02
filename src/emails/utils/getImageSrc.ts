967c346269e73b6479224b64a2332552c4c87d15
Zinazero <jacobhexamer@gmail.com>
2025-10-02 00:43:29 -0400

    fix: using public image because gmail doesn't display base64 images

--- src/emails/utils/getImageSrc.ts
+++ src/emails/utils/getImageSrc.ts
@@ -1,21 +1,5 @@
-import fs from 'fs';
-import path from 'path';
-
-const isDev = process.env.NODE_ENV !== 'production';
-const clientDevUrl = process.env.CLIENT_DEV_URL;
-
-export const getLogoSrc = (): string => {
-	if (isDev && clientDevUrl) {
-		return `${clientDevUrl}/images/cobalt-logo192.png`;
-	}
-
-	const filePath = path.join(
-		process.cwd(),
-		'src',
-		'emails',
-		'assets',
-		'cobalt-logo192.png'
-	);
-	const base64 = fs.readFileSync(filePath, 'base64');
-	return `data:image/png;base64,${base64}`;
+export const getLogoSrc = () => {
+	const isDev = process.env.NODE_ENV !== 'production';
+	const baseUrl = isDev ? 'http://localhost:3000' : 'https://cobaltsoft.ca';
+	return `${baseUrl}/images/cobalt-logo192.png`;
 };
