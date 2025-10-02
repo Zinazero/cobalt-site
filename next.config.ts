a5d0167753e34c1053fd6334e0f4bb1fcb994ce3
Zinazero <jacobhexamer@gmail.com>
2025-10-01 22:58:39 -0400

    feat: testing CSP

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,28 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+	async headers() {
+		return [
+			{
+				source: '/(.*)',
+				headers: [
+					{
+						key: 'Content-Security-Policy',
+						value: `
+              default-src 'self';
+              script-src 'self' 'unsafe-inline';
+              style-src 'self' 'unsafe-inline';
+              img-src 'self' data:;
+              font-src 'self';
+              connect-src 'self' https:;
+            `
+							.replace(/\s{2,}/g, ' ')
+							.trim(),
+					},
+				],
+			},
+		];
+	},
 };
 
 export default nextConfig;
