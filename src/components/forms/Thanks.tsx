a619f08dd69cb6d76a062643e0728801607aa928
Zinazero <jacobhexamer@gmail.com>
2025-10-02 18:05:22 -0400

    fix: replaced straight quote with smart quote

--- src/components/forms/Thanks.tsx
+++ src/components/forms/Thanks.tsx
@@ -10,7 +10,7 @@ export const Thanks = ({ onClick }: ThanksProps) => (
 		<h4 className='text-4xl font-semibold text-cobalt mb-2'>
 			Thanks for reaching out!
 		</h4>
-		<p className='!text-xl'>We'll be in touch shortly.</p>
+		<p className='!text-xl'>We’ll be in touch shortly.</p>
 		<Button
 			onClick={onClick}
 			className='bg-cobalt hover:bg-cobalt-hover active:scale-95 text-white mt-14 w-full max-w-70'
