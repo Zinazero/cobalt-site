802b63a189552a878f545ed90d9e90a9998465cf
Zinazero <jacobhexamer@gmail.com>
2026-01-06 15:01:37 -0500

    chore: update copyright year

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
     <footer className="bg-gradient-to-l from-abyss to-cobalt shadow-sm text-white text-sm py-3 text-center">
-      <span>&copy; {new Date().getFullYear()} Cobalt Software Solutions Inc. All rights reserved.</span>
+      <span>&copy; 2026 Cobalt Software Solutions Inc. All rights reserved.</span>
     </footer>
   );
 };
